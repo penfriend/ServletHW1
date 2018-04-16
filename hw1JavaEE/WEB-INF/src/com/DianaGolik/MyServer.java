cbd12a2a90243fa67defe28d782e7aeefd6e90f7
Diana Golik <penfriend@mail.ru>
2018-04-17 00:18:27 +0200

    Add files via upload

---
+++ hw1JavaEE/WEB-INF/src/com/DianaGolik/MyServer.java
@@ -0,0 +1,92 @@
+package com.DianaGolik;
+
+ 
+
+import java.io.*;
+
+import javax.servlet.*;
+
+import javax.servlet.http.*;
+
+public class MyServer extends HttpServlet {
+
+private static final long serialVersionUID = 1L;
+
+protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+
+throws ServletException, IOException {
+
+response.setContentType("text/html;charset=UTF-8");
+PrintWriter out = response.getWriter();
+
+try {
+
+out.println("<html>");
+out.println("<head>");
+
+out.println("<title>Registration form</title>");
+
+out.println("</head>");
+
+out.println("<body>");
+String name = request.getParameter("name");
+String password = request.getParameter("password");
+if (name.equals("Hello") && password.equals("world")) {
+    out.println("Congratulation! You have succesfully entered with such parameters:");
+    out.println(" username = " + name + "<br>");
+    out.println(" password = " + password);
+} 
+ else if (name.equals("") && password.equals("")) {
+         out.println("Please enter your login and password.");  
+       }
+	   else {
+		out.println("Your login or password is invalid.");
+	   }
+		
+out.print("<form action=\"");
+out.print("RegForm\" ");
+out.println("method=POST>");
+/*out.println("Username: ");
+out.println("<input type=text size=20 name=name>");
+out.println("<br>");
+out.println("Password: ");
+out.println("<input type=text size=20 name=password>");
+out.println("<br>");*/
+out.println("<input type=submit value=\"Return\">");
+out.println("</form>");
+
+out.println("</body>");
+
+out.println("</html>");
+
+ 
+
+} finally {
+
+out.close();
+
+}
+
+}
+
+ 
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+
+throws ServletException, IOException {
+
+processRequest(request, response);
+
+}
+
+ 
+
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+
+throws ServletException, IOException {
+
+processRequest(request, response);
+
+}
+
+}
